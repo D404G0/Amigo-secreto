d917fb92935b8aa1132b3b8645c2fe728057fcb2
Diego <diegol-patinof@unilibre.edu.co>
2025-02-07 21:00:20 -0500

    En el archivo .js se agrega la estructura para que funcione el juego, ya esta en su totalidad

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let lista_amigos = []
+
+//recibe el elemento y texto para que sea dinamico
+function texto_para_elemento(elemento, texto){
+    let variableHTML = document.getElementById(elemento);
+    variableHTML.innerHTML = texto;
+    return;
+}
+
+//funcion para limpiar el campo del input
+function limpiar_input(){
+    document.getElementById(`amigo`).value = '';
+};
+
+//Funcion donde recibe el valor del input
+function añadir_amigo(){
+    nombre = document.getElementById(`amigo`).value;
+
+    if (isNaN(nombre)){
+        if (nombre === ''){
+            alert("Debe ingresar un string. ");
+    
+        }else{
+            lista_amigos.push(nombre);
+        }
+
+    }else{
+        alert("Debe ingresar un string. ");
+    }
+
+    texto_para_elemento(`lista_Amigos`, `Amigos: \n ${lista_amigos} \n`);
+
+    console.log(lista_amigos);
+
+    limpiar_input();
+    return
+}
+
+
+//Funcion para sortear amigos
+function escoger_amigo(){
+    if (!lista_amigos || lista_amigos.length === 0){
+        alert("Ingrese mas de dos amigos para escoger su amigo secreto. ")
+    }else{
+        let amigo_escogido = lista_amigos[Math.floor(Math.random() * lista_amigos.length)];
+        texto_para_elemento(`resultado`, `Su amigo secreto es ${amigo_escogido}`);
+    }
+}
+
+
